0a3e00795f80862c811b3d08f265ea3df1141cf5
김 남규 <skarb629@naver.com>
2020-09-10 22:56:09 +0900

    config file add

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # node-tutorial
+
+
+# Client - Server 통신방법
+클라이언트에서 서버에 요청을 하면 Body-parser::Dependency => npm install body-parser --save
+
+Register Router 만든다.
+
--- index.js
+++ index.js
@@ -2,13 +2,18 @@
 const express = require('express') //express module 가져옴
 const app = express()
 const port = 5000 //5000포트
+const bodyParser = require('body-parser');
+const {User} = require('./model/User');
+const config = require('./config/key');
 
-//몽고디비 생성및 몽고디비 유저 생성 southkyu / qwer1234
-
+// applicatiion/ x-www-form-urlencoded 분석해서 가져옴
+app.use(bodyParser.urlencoded({extended:true}));    
+// application/json 가져오기
+app.use(bodyParser.json());
 
 //몽구스를 이용하여 몽고디비 연결
 const mongoose = require("mongoose")
-mongoose.connect('mongodb+srv://sothkyu:qwer1234@nodedbs.q5h1l.mongodb.net/test?retryWrites=true&w=majority', {
+mongoose.connect(config.mongoURI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useCreateIndex: true, 
@@ -16,6 +21,22 @@ mongoose.connect('mongodb+srv://sothkyu:qwer1234@nodedbs.q5h1l.mongodb.net/test?
 }).then(() => console.log("MongoDB Connected..."))
   .catch(err => console.log(err))
 
+
 app.get('/', (req, res) => res.send('Hello World!'))
+
+//회원가입 라우터
+app.post('/register', (req, res) => {
+    //클라이언트에서 보내주는 데이터들을 가져오면
+    //그것들을 데이터베이스에 넣어준다.
+    // req.body 안에 객체 형식으로 데이터를 받아옴 body-parser에 의하여
+    const user = new User(req.body)
+
+    user.save((err, userInfo) => {
+        if(err) return res.json({success:false, err})
+        return res.status(200).json({
+            success:true
+        })
+    }) 
+})
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
