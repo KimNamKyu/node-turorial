df30350a911414e27255541905ada77b1874ec4b
김 남규 <skarb629@naver.com>
2020-09-11 00:06:46 +0900

    login error

--- model/User.js
+++ model/User.js
@@ -1,5 +1,7 @@
 const mongoose = require('mongoose');
-
+const bcrypt = require('bcrypt');
+const saltRounds = 10
+const jwt = require('jsonwebtoken');
 const userSchema = mongoose.Schema({
     name: {
         type: String,
@@ -30,6 +32,47 @@ const userSchema = mongoose.Schema({
         type: Number
     }
 })
+
+userSchema.pre('save', function(next){
+    //비밀번호를 암호화시킨다. 저장하기전에
+    let user = this;
+
+    if(user.isModified('password')){
+        bcrypt.genSalt(saltRounds, function(err, salt){
+            if(err) return next(err)
+            
+            bcrypt.hash(user.password, salt, function(err,hash){
+                if(err) return next(err)
+                user.password = hash    //hash 된 비밀번호로 변경
+                next()
+            })
+        })
+    }else{
+        next()
+    }
+}) //mongoose method
+
+userSchema.methods.comparePassword = function(plainPassword, cb) {
+    //plainPassword 12341234  암호화된 비밀번호 >> 복호화X 암호화해서 비교
+    console.log(plainPassword)
+    bcrypt.compare(plainPassword, this.password, function(err, isMatch) {
+        console.log(err)
+        if(err) return cb(err),
+            cb(null, isMatch)
+    })
+}
+
+userSchema.methods.generateToken = function(cb) {
+    //jsonwebtoken을 활용하여 토큰 생성
+    let user = this;
+    let token = jwt.sign(user._id.toHexString(), 'secrectToken')
+
+    user.token = token
+    user.save(function(err, user){
+        if(err) return cb(err)
+        cb(null, user)
+    })
+}
 
 const User = mongoose.model('User', userSchema)  //모델을 스키마에 넣어준다.
 module.exports = {User}
